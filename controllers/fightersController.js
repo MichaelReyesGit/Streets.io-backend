ae092c37b4cfe87c7eb1c668c09c17246e236f01
michael reyes <draftiercrayon4@gmail.com>
2023-08-21 08:13:52 -0400

    completed fightersController and added validations

---
+++ controllers/fightersController.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllFighters,
+  fighterById,
+  createFighter,
+  deleteFighter,
+  updateFighterById,
+} = require("../queries/fighters");
+
+const {
+  checkName,
+  checkBoolean,
+  validateURL,
+} = require("../validations/checkFighters");
+
+router.get("/", async (req, res) => {
+  const allFighters = await getAllFighters();
+
+  if (Array.isArray(allFighters)) {
+    res.json(allFighters);
+  } else {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const fighter = await fighterById(id);
+
+  if (fighter.length === 0) {
+    res.status(404).json({ error: "not found" });
+  } else {
+    res.json(pet[0]);
+  }
+});
+
+router.post("/", checkName, checkBoolean, validateURL, async (req, res) => {
+  const newFighter = await createFighter(req.body);
+  res.json(newFighter);
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedFighter = await deleteFighter(req.params.id);
+
+  if (deletedFighter.length === 0) {
+    return res.status(404).json({ message: "No data found!", error: true });
+  } else {
+    return res.json(deletedFighter[0]);
+  }
+});
+
+router.put("/:id", checkName, checkBoolean, validateURL, async (req, res) => {
+  const updatedFighter = await updateFighterById(req.params.id, req.body);
+  console.log(updatedFighter);
+  if (updatedFighter.length === 0) {
+    res.status(404).json({ message: "not found!", error: true });
+  } else {
+    res.json(updatedFighter[0]);
+  }
+});
+
+module.exports = router;
---
+++ validations/checkFighters.js
@@ -0,0 +1,32 @@
+const checkName = (req, res, next) => {
+  if (!req.body.name) {
+    res.status(400).json({ error: "Name is required" });
+  } else {
+    next();
+  }
+};
+
+const checkBoolean = (req, res, next) => {
+  const { is_your_character } = req.body;
+
+  if (typeof is_your_character !== "boolean") {
+    res.status(400).json({ error: "is_favorite must be a boolean value" });
+  } else {
+    next();
+  }
+};
+
+const validateURL = (req, res, next) => {
+  if (
+    req.body.url.substring(0, 7) === "http://" ||
+    req.body.url.substring(0, 8) === "https://"
+  ) {
+    return next();
+  } else {
+    res.status(400).json({
+      error: "You forgot to start your photo url with http:// or https://",
+    });
+  }
+};
+
+module.exports = { checkName, checkBoolean, validateURL };
